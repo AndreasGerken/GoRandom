9019b3e49c516f4d6cead8215fadc4b9c224d797
Andreas Gerken <andi.gerken@gmail.com>
2018-03-14 20:24:53 +0900

    Added lines to change easily

--- go_random.py
+++ go_random.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+
+# TO EXECUTE THIS PROGRAM IN REPL.IT CLICK ON THE RUN BUTTON ABOVE
+
 import sys
 import argparse
 import numpy as np
@@ -14,20 +17,32 @@ args = parser.parse_args()
 
 stones_per_player = args.stones_per_player
 board_size = args.board_size
+place_border_stones = args.place_border_stones
 
+# UNCOMMENT THIS TO CREATE YOUR OWN CONFIGURATION WITHOUT PASSING ARGUMENTS
+# stones_per_player = 5
+# board_size = 13
+# place_border_stones = True
+
+# The board is technically two spaces smaller if the boarder is not used
 if args.place_border_stones:
     board_size_no_border = board_size
 else:
     board_size_no_border = board_size - 2
 
-if stones_per_player > board_size * board_size:
+# Sanity check if all stones fit on the board
+if stones_per_player > board_size_no_border * board_size_no_border:
     raise Exception("The stones don't fit on the field")
 
+# Draw random stones from the board
 random_stones = np.random.choice(
     board_size_no_border * board_size_no_border, stones_per_player * 2, replace=False)
+
+# Split the chosen stones between black and white
 randomsB = random_stones[:stones_per_player]
 randomsW = random_stones[stones_per_player:]
 
+# Star positions for standard board sizes
 stars = {9: [[2, 2], [2, 7], [4, 4], [7, 2], [7, 7]],
          13: [[3, 3], [3, 9], [6, 6], [9, 3], [9, 9]],
          19: [[3, 3], [3, 9], [3, 15], [9, 3], [9, 9], [9, 15], [15, 3], [15, 9], [15, 15]]}
@@ -35,6 +50,7 @@ stars = {9: [[2, 2], [2, 7], [4, 4], [7, 2], [7, 7]],
 # Iterate over the whole field
 for i in range(board_size):
     for j in range(board_size):
+        # Check if position is on border
         on_border = (i == 0 or j == 0 or i == board_size - 1 or j ==
                      board_size - 1) and not args.place_border_stones
         # Get an index from row and column
