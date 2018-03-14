b39adb68b86edbe7baceb6b8e3322752941d8c44
Andreas Gerken <andi.gerken@gmail.com>
2018-03-14 20:08:08 +0900

    Added Feature to prevent stones on the border lines.

--- go_random.py
+++ go_random.py
@@ -4,22 +4,26 @@ import argparse
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--stones_per_player",
-                    help="Number of random stones per player, default = 10", type=int, default=10)
+parser.add_argument('-s', '--stones_per_player',
+                    help='Number of random stones per player, default = 10', type=int, default=10)
 parser.add_argument(
-    "-f", "--field_size", help="Field size, for the standard sizes (9, 13, 19), the star points are shown, default = 19", type=int, default=19)
+    '-f', '--board_size', help='Field size, for the standard sizes (9, 13, 19), the star points are shown, default = 19', type=int, default=19)
+parser.add_argument('-b', '--place_border_stones', help="By default, no stones will be placed on the border. This option allows it", action='store_true')
 args = parser.parse_args()
 
-
 stones_per_player = args.stones_per_player
-field_size = args.field_size
+board_size = args.board_size
 
+if args.place_border_stones:
+    board_size_no_border = board_size
+else:
+    board_size_no_border = board_size - 2
 
-if stones_per_player > field_size * field_size:
+if stones_per_player > board_size * board_size:
     raise Exception("The stones don't fit on the field")
 
 random_stones = np.random.choice(
-    field_size * field_size, stones_per_player * 2)
+    board_size_no_border * board_size_no_border, stones_per_player * 2, replace=False)
 randomsB = random_stones[:stones_per_player]
 randomsW = random_stones[stones_per_player:]
 
@@ -28,22 +32,26 @@ stars = {9: [[2, 2], [2, 7], [4, 4], [7, 2], [7, 7]],
          19: [[3, 3], [3, 9], [3, 15], [9, 3], [9, 9], [9, 15], [15, 3], [15, 9], [15, 15]]}
 
 # Iterate over the whole field
-for i in range(field_size):
-    for j in range(field_size):
-
+for i in range(board_size):
+    for j in range(board_size):
+        on_border = (i == 0 or j == 0 or i == board_size - 1 or j == board_size - 1) and not args.place_border_stones
         # Get an index from row and column
-        index = i * field_size + j
+        if args.place_border_stones:
+            index = i * board_size + j
+        else:
+            # Move the indices one up to center the board
+            index = (i - 1) * board_size_no_border + (j - 1)
 
         # Check if the index is in the selected fields
-        if index in randomsB:
-            sys.stdout.write('B')
-        elif index in randomsW:
-            sys.stdout.write('W')
+        if index in randomsB and not on_border:
+            sys.stdout.write('B ')
+        elif index in randomsW and not on_border:
+            sys.stdout.write('W ')
         else:
             # Check if the field is a star
-            if field_size in stars and [i, j] in stars[field_size]:
-                sys.stdout.write('*')
+            if board_size in stars and [i, j] in stars[board_size]:
+                sys.stdout.write('* ')
             else:
-                sys.stdout.write('.')
+                sys.stdout.write('. ')
     # New line
     sys.stdout.write('\n')
